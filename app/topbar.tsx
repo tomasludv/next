f8ea536933db0f4f28e36682a2919c3f22902e6b
pilnystudent <tomasludvik@seznam.cz>
2024-01-28 12:05:51 +0100

    。

--- app/topbar.tsx
+++ app/topbar.tsx
@@ -23,16 +23,17 @@ export default function Topbar() {
     return (
         <>
             <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
-                <div className="relative flex flex-1">
-                    <div className="p-4">
-                        {address &&
-                            <button
-                                type="button"
-                                className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                                onClick={approve}
-                            >USDC Approve</button>
-                        }
-                    </div>
+                <div className="flex items-center flex-1 gap-x-4 lg:gap-x-6">
+                    {address &&
+                        <a href="https://gho.aave.com/faucet/" className="font-semibold text-indigo-500">Mint USDC</a>
+                    }
+                    {address &&
+                        <button
+                            type="button"
+                            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            onClick={approve}
+                        >Approve USDC</button>
+                    }
                 </div>
                 <div className="flex items-center gap-x-4 lg:gap-x-6">
                     <ConnectButton />
